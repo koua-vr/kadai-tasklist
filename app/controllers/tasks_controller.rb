8ed6a6e3d319e1d501e4f15735aa95b59be75b49
kouhei.toda <kouhei.toda@techacademy.jp>
2018-08-14 12:51:37 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,12 +14,15 @@ class TasksController < ApplicationController
 
 # 以下、resourceで生成される全てのアクションを実装
 
+  # 共通処理
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  # 各アクション
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -39,12 +42,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
-
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
@@ -55,7 +55,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
@@ -67,9 +66,14 @@ class TasksController < ApplicationController
 # #########################################
   private
 
+  # taskの取得
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
   # Strong Parameter
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 # #########################################
 
